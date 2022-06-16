0d6eecc93f8dbb1d21c53e80d72cc2d5ec910803
TMillsap04 <tmmillsap@student.fullsail.edu>
2022-06-15 19:01:17 -0500

    Working on router.post segment

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -37,64 +37,22 @@ router.post('/login', (req, res, next) => {
     //  findUser by email address findOne{email: _id}
     // if not found return Auth Failed
     // else
-    // compare passwords using bcrypt get error or result of true or false
-    // if error return err.message
-    // else return response
-        
-        bcrypt.hash(password, 10, (err, hash) => {
-            if (err) {
-                res.status(500).json({
-                    message: err.message,
-                });
-            } else {
-                newUser.save()
-                    .then(result => {
-                        res.status(201).json({
-                        message: 'Signup - POST',
-                        user: {
-                            firstName: req.body.firstName,
-                            email: req.body.email,
-                            password: hash, 
-                        }
-                    })})
-                    .catch(err => {
-                        res.status(500).json({
-                            error: {
-                                message: 'User not saved'
-                            }
-                    })});
-};
+    // user returned with user info and a HASHED PW     
+    bcrypt.compare(req.body.password, req.body.hash, (err, result) => {
+        if(err)return res.status(501).json({message: 'Authorization Failed' })
+        if(result){
+            res.status(200).json({
+            message: 'Login - POST,  Authorization Successful',
+            result: result,
+            name: firstName
+            })
+        }
+    });
+});
 
 router.get('/profile', (req,res, next) => {
-   const userEmail = req.params.email;
    
-   user.findById({
-       _id: userEmail
-   })
-   .then(result => {
-       if (!email) {
-           return res.status(401).json({
-               message: 'No email found'
-           })
-       } else {
-        bcrypt.compare(req.body.password, req.body.hash, (err, result) => {
-             if(err)return res.status(409).json({
-                message: 'Authorization Failed'
-                })
-             else {
-                return res.status(200).json({
-                    message: 'Profile - GET,  Authorization Successful'
-                })
-            }
-        })}})
-    .catch(err => {
-         res.status(500).json({
-            error: {
-                message: 'Error occurred'
-            }
-        });
-    })
    
-})})});
+});
 
 module.exports = router;
